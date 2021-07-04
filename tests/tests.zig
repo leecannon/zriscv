fb9d5f2802ec6ecb102aab4a58cf56a70614af4f
Lee Cannon <leecannon@leecannon.xyz>
2021-07-04 22:28:22 +0100

    Set timeout in tests

--- tests/tests.zig
+++ tests/tests.zig
@@ -6,12 +6,43 @@ test "rv64ui_p_add" {
     try runTest("rv64ui_p_add.bin");
 }
 
+test "rv64ui_p_addi" {
+    try runTest("rv64ui_p_addi.bin");
+}
+
+const TestContext = struct {
+    file_name: []const u8,
+    the_error: ?anyerror = null,
+    reset_event: *std.Thread.ResetEvent,
+};
+
 fn runTest(file_name: []const u8) !void {
+    var reset_event = try std.testing.allocator.create(std.Thread.ResetEvent);
+    defer std.testing.allocator.destroy(reset_event);
+
+    try reset_event.init();
+    defer reset_event.deinit();
+
+    var context = TestContext{ .file_name = file_name, .reset_event = reset_event };
+
+    var runner_thread = try std.Thread.spawn(executeTest, &context);
+    defer runner_thread.wait();
+
+    if (reset_event.timedWait(std.time.ns_per_s * 2) == .timed_out) {
+        return error.TestTimedOut;
+    }
+
+    if (context.the_error) |err| return err;
+}
+
+fn executeTest(context: *TestContext) !void {
+    defer context.reset_event.set();
+
     const file_contents = blk: {
         var resource_dir = try std.fs.openDirAbsolute(resource_path, .{});
         defer resource_dir.close();
 
-        var file = try resource_dir.openFile(file_name, .{});
+        var file = try resource_dir.openFile(context.file_name, .{});
         defer file.close();
 
         break :blk try file.readToEndAlloc(std.testing.allocator, std.math.maxInt(usize));
@@ -20,8 +51,12 @@ fn runTest(file_name: []const u8) !void {
 
     var cpu = zriscv.Cpu{ .memory = file_contents };
     cpu.run() catch |err| switch (err) {
-        error.ExecutionOutOfBounds => if (cpu.x[10] != 0) return err,
-        else => return err,
+        error.ExecutionOutOfBounds => {
+            if (cpu.x[10] != 0) {
+                context.the_error = err;
+            }
+        },
+        else => context.the_error = err,
     };
 }
 

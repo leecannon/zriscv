7f4f3f0b1f0d077111163a222917b404b45a4744
Lee Cannon <leecannon@leecannon.xyz>
2022-09-14 22:28:05 +0100

    implement LHU

--- src/execution.zig
+++ src/execution.zig
@@ -931,7 +931,75 @@ fn execute(
                 hart.pc += 4;
             }
         },
-        .LHU => return instructionExecutionUnimplemented("LHU"), // TODO: LHU
+        .LHU => {
+            const z = lib.traceNamed(@src(), "LHU");
+            defer z.end();
+
+            // I-type
+
+            const rd = instruction.rd();
+
+            if (rd != .zero) {
+                const rs1 = instruction.rs1();
+                const rs1_value = hart.x[@enumToInt(rs1)];
+                const imm = instruction.i_imm.read();
+
+                const address = addSignedToUnsignedWrap(rs1_value, imm);
+
+                if (has_writer) {
+                    try writer.print(
+                        \\LHU - base: {}, dest: {}, imm: 0x{x}
+                        \\  load 2 bytes into {} from memory ( {}<0x{x}> + 0x{x} ) = 0x{x}
+                        \\
+                    , .{
+                        rs1,
+                        rd,
+                        imm,
+                        rd,
+                        rs1,
+                        rs1_value,
+                        imm,
+                        address,
+                    });
+                }
+
+                if (actually_execute) {
+                    const memory = if (options.execution_out_of_bounds_is_fatal)
+                        try hart.loadMemory(16, address)
+                    else blk: {
+                        break :blk hart.loadMemory(16, address) catch |err| switch (err) {
+                            error.ExecutionOutOfBounds => {
+                                // TODO: Pass `.LoadAccessFault` once `throw` is implemented
+                                try throw(mode, hart, {}, 0, writer, true);
+                                return true;
+                            },
+                            else => |e| return e,
+                        };
+                    };
+
+                    hart.x[@enumToInt(rd)] = memory;
+                }
+            } else {
+                if (has_writer) {
+                    const rs1 = instruction.rs1();
+                    const imm = instruction.i_imm.read();
+
+                    try writer.print(
+                        \\LHU - base: {}, dest: {}, imm: 0x{x}
+                        \\  nop
+                        \\
+                    , .{
+                        rs1,
+                        rd,
+                        imm,
+                    });
+                }
+            }
+
+            if (actually_execute) {
+                hart.pc += 4;
+            }
+        },
         .SB => return instructionExecutionUnimplemented("SB"), // TODO: SB
         .SH => return instructionExecutionUnimplemented("SH"), // TODO: SH
         .SW => {
